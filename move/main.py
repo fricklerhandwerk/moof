620482f35dab4e1ff5cc1b3885f7b1ac8be5fe2b
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-06-20 21:16:06 +0200

    formatting

--- move/main.py
+++ move/main.py
@@ -1,18 +1,23 @@
 import signal
-from blessed import Terminal
 from functools import partial
 
+from blessed import Terminal
+
 echo = partial(print, end='', flush=True)
+
 
 def center(t, s):
     x = (t.width - len(s))//2
     return t.move_x(x) + s
 
+
 def middle(t, s):
     y = (t.height - s.count('\n'))//2
     return t.move_y(y) + s
 
+
 draw = False
+
 
 def main():
     t = Terminal()
@@ -25,21 +30,22 @@ def main():
                     break
                 blit(t, val)
 
+
 def blit(t, val):
-  global draw
-  if val.code == t.KEY_ENTER:
-      draw = not draw
-      if draw:
-          with t.location():
-              echo(t.standout(' '))
-  if val.code in [t.KEY_DOWN, t.KEY_UP, t.KEY_LEFT, t.KEY_RIGHT]:
-      echo(val)
-      if draw:
-          with t.location():
-              echo(t.standout(' '))
-      return
-  if val.code in [t.KEY_DELETE, t.KEY_BACKSPACE]:
-      echo(t.clear)
-      return
-  with t.location():
-      echo(val)
+    global draw
+    if val.code == t.KEY_ENTER:
+        draw = not draw
+        if draw:
+            with t.location():
+                echo(t.standout(' '))
+    if val.code in [t.KEY_DOWN, t.KEY_UP, t.KEY_LEFT, t.KEY_RIGHT]:
+        echo(val)
+        if draw:
+            with t.location():
+                echo(t.standout(' '))
+        return
+    if val.code in [t.KEY_DELETE, t.KEY_BACKSPACE]:
+        echo(t.clear)
+        return
+    with t.location():
+        echo(val)
